15aedae73d57badade6b0424e53f32d21359037b
Zivoric <52415929+zivoric@users.noreply.github.com>
2023-01-09 14:24:57 -0500

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 //import logo from './logo.svg';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import {Route, Routes} from "react-router-dom";
 //import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Routes>
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/register"} element={<Register />} />
+      </Routes>
     </div>
   );
 }
