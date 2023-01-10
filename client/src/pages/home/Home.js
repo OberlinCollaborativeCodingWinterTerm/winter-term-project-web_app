cf03038106804421964651130f0254c0f8323fe4
JoeSangiolo <jsangiol@oberlin.edu>
2023-01-10 18:18:27 -0500

    New Files
    Created Home.js, Profile.js, and Sidebar.js

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import "./home.scss";
+
+
+const Home = () => {
+    return (
+        <div className="register">
+            <div className="card">
+                This is a test. This is also the home page currently
+            </div>
+        </div>
+    )
+}
+
+export default Home
