5092fb3b4afb12ef161a7e1b37e6a7480eae90a1
Zivoric <52415929+zivoric@users.noreply.github.com>
2023-01-11 13:10:12 -0500

    Added default route with Header

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import {Route, Routes} from "react-router-dom";
-//import './App.css';
+import {Header} from "./components/header/Header";
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/register"} element={<Register />} />
-      </Routes>
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*">
+                <Route index={true} element={<Header/>} />
+                {/* put routes for other pages under here */}
+            </Route>
+        </Routes>
     </div>
   );
 }
