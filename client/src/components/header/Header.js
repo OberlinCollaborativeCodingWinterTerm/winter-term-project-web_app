cdd8f570c1b092083564459db41a7304ac9509b7
Zivoric <52415929+zivoric@users.noreply.github.com>
2023-01-11 15:04:15 -0500

    added base user menu to header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.scss";
-import {Navbar, Nav} from "react-bootstrap";
+import {Navbar, Nav, NavDropdown} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 
 export class Header extends React.Component {
@@ -21,6 +21,13 @@ export class Header extends React.Component {
                         </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
+                <Nav>
+                    <NavDropdown className="me-2" align="end" title={<img alt="" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" className="rounded-circle my-auto" width="32" height="32"></img>}>
+                        <LinkContainer to="/settings">
+                            <NavDropdown.Item>Settings</NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                </Nav>
             </Navbar>
         );
     }
