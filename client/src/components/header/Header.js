adc587009262e1a6b2430c51242051e77ba7b8c4
Zivoric <52415929+zivoric@users.noreply.github.com>
2023-01-11 14:38:23 -0500

    created actual base header bar

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Header.scss";
+import {Navbar, Nav} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
 export class Header extends React.Component {
     render() {
         return (
-            <div className="navbar">
-
-            </div>
+            <Navbar>
+                <LinkContainer to="/">
+                    <Navbar.Brand>StudyBuddies</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav className="me-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/classes">
+                            <Nav.Link>Classes</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
