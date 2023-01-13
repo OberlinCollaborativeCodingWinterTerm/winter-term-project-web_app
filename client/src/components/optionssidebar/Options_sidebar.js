98d9370241adaca33786b18f492d5702ef1282d8
saidaibrg <ibrag.saida@gmail.com>
2023-01-12 21:10:36 -0500

    Set up icons for vertical sidebar and add styling

--- client/src/components/optionssidebar/Options_sidebar.js
+++ client/src/components/optionssidebar/Options_sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Options_sidebar.css";
 import { SidebarData } from "./Options_sidebarData";
 
+
 // TODO: Add the icons to <div>{val.icon} through Material Icon latter
 
 export class Options_sidebar extends React.Component {
@@ -12,8 +13,7 @@ export class Options_sidebar extends React.Component {
         {SidebarData.map((val, key)=>{
         return (
           <li className="row" key={key} onClick={()=> {window.location.pathname=val.link}}>
-          {/* <div>{val.icon}</div>{" "} */}
-          
+          <div>{val.icon}</div>{" "} 
           <div>{val.title}</div>{" "}
           </li>
           );
--- client/src/components/optionssidebar/Options_sidebarData.js
+++ client/src/components/optionssidebar/Options_sidebarData.js
@@ -1,26 +1,30 @@
 import React from "react"
+// Icons Ref: https://react-icons.github.io/react-icons/icons?name=ai
+import { AiFillHome} from 'react-icons/ai';
+import { AiFillQuestionCircle} from 'react-icons/ai';
+import { AiOutlineUsergroupAdd} from 'react-icons/ai';
+import { AiFillNotification} from 'react-icons/ai';
 
+// Link attributes are subject to change
 export const SidebarData=[
     {
         title:"Everything",
-        // icon:<Icon />, 
+        icon: <AiFillHome />,
         link: "/viewpost"
-
     }, 
     {
         title:"Questions",
-        // icon:<Icon />, 
-        link: "/login"
-
+        icon: <AiFillQuestionCircle />, 
+        link: "/viewpost"
     }, 
     {
         title:"Study Groups",
-        // icon:<Icon />, 
-        link: "/"
+        icon:<AiOutlineUsergroupAdd />, 
+        link: "/viewpost"
     },
     {
         title:"Announcements",
-        // icon:<Icon />, 
+        icon:<AiFillNotification />, 
         link: "/"
     }, 
 
