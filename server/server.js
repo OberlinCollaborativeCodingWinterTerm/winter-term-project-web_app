35d4214a4d017d28f378ca5d006bb74175485a7c
saidaibrg <ibrag.saida@gmail.com>
2023-01-17 21:10:52 -0500

    Connect to MongoDB database, set up routes, and partially edit the sidebar

--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
-const express=require('express')
+const express=require('express');
+const app = express();
+const cors=require("cors");
 
-const app = express()
+require("dotenv").config({path: "./config.env"});
+const port=process.env.PORT || 5000;
 
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// require database connection 
+const dbConnect = require("./db/dbConnect");
+
+app.listen(port, ()=>{ 
+    // perform connection to the database once the server starts
+    dbConnect.connectToServer(function (err){
+        if (err) console.error(err);
+    }); 
+    console.log('Server started on port: 5000');
+});
+
+
+// simple example route
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]});  // Response that is sent to App.js after the GET request 
 })
@@ -15,4 +35,8 @@ app.put("/", (req,res)=>{
 })
 // TODO: Create more endpoints with app.get("/yourDesiredRoute")
 
-app.listen(5000, ()=>{ console.log("Server started on port 5000") })
+
+
+
+
+ 
