56599f50d0d8da3db7bc11f182c72ed192b64d4a
Zivoric <52415929+zivoric@users.noreply.github.com>
2023-01-20 14:47:15 -0500

    grayed out post functionality; added link to post preview

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,12 +3,14 @@ import React from "react";
 import {Badge, Button, Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {AiFillQuestionCircle} from "react-icons/ai";
+import {LinkContainer} from "react-router-bootstrap";
 
 export default class Post extends React.Component {
     constructor(props, defaultState) {
         super(props);
         this.state = {
             preview: !!(props.preview),
+            grayed: false,
             ...defaultState
         };
         this.postContent = this.postContent.bind(this);
@@ -38,23 +40,25 @@ export default class Post extends React.Component {
     render() {
         return (
             <>
-                <Container className={this.state.preview ? "container-md bg-white my-4 p-4 shadow-sm rounded-4" : "container-md bg-white mt-5 p-4 shadow-sm rounded-4"}>
-                    <p className="mb-1 text-secondary d-flex align-items-start">
-                        <small className="me-auto d-flex align-items-center"><img alt="" src={this.props.user.icon} style={{ width: '1.5em', height: '1.5em' }} className="rounded-circle"  /> <span className="px-1"><Link to={`/users/${this.props.user.username}`} className="text-decoration-none link-secondary">{this.props.user.firstName + " " + this.props.user.lastName}</Link></span><Badge pill bg="student">{this.props.user.flair.toUpperCase()}</Badge>
-                        <span className="px-1">&middot; posted in <Link className="text-primary" to={"/courses/" + this.props.course}>{this.props.course}</Link></span></small>
-                        <this.iconDisplay className="text-quaternary" size="2em" />
-                    </p>
-                    <span className={`pe-5 ${this.state.preview ? "h3" : "h2"}`}>
-                        <span className="me-2">{this.props.title}</span>
-                        {(this.props.flair != null) && <Badge pill bg={this.props.flair.toLowerCase()}>{this.props.flair.toUpperCase()}</Badge>}
-                    </span>
-                    <p className={this.state.preview ? "text-secondary" : "text-body"}>{this.props.description}</p>
-                    <this.postContent {...this.props} />
-                    <div className="d-flex justify-content-md-end">{this.props.tags.map((tag) => <Link to="" key={tag}><Button style={{paddingRight: "0.75em", paddingLeft: "0.75em"}} className="mx-1 badge rounded-pill" variant="secondary">{tag}</Button></Link>)}</div>
-                    {/* user, title, description, tags, like button is universal */
-                     /* post content is what differentiates different types of posts */
-                     /* feedback content is what is under the post, being comments, answers, group chat, etc. */}
-                </Container>
+                <LinkContainer to={this.state.preview ? "/post/" + this.props.postId : ""}>
+                    <Container className={this.state.preview ? "container-md bg-white my-4 p-4 shadow-sm rounded-4" : "container-md bg-white mt-5 p-4 shadow-sm rounded-4"}>
+                        <p className={`mb-1 text-secondary d-flex align-items-start`}>
+                            <small className="me-auto d-flex align-items-center"><img alt="" src={this.props.user.icon} style={{ width: '1.5em', height: '1.5em' }} className="rounded-circle"  /> <span className="px-1"><Link to={`/users/${this.props.user.username}`} className="text-decoration-none link-secondary">{this.props.user.firstName + " " + this.props.user.lastName}</Link></span><Badge pill bg="student">{this.props.user.flair.toUpperCase()}</Badge>
+                            <span className="px-1">&middot; posted in <Link className="text-primary" to={"/courses/" + this.props.course}>{this.props.course}</Link></span></small>
+                            <this.iconDisplay className="text-quaternary" size="2em" />
+                        </p>
+                        <span className={`pe-5 ${this.state.preview ? "h3" : "h2"}`}>
+                            <span className={`me-2 ${this.state.grayed ? "text-tertiary" : ""}`}>{this.props.title}</span>
+                            {(this.props.flair != null) && <Badge pill bg={this.props.flair.toLowerCase()}>{this.props.flair.toUpperCase()}</Badge>}
+                        </span>
+                        <p className={this.state.preview ? (this.state.grayed ? "text-tertiary" : "text-secondary") : "text-body"}>{this.props.description}</p>
+                        <this.postContent {...this.props} />
+                        <div className="d-flex justify-content-md-end">{this.props.tags.map((tag) => <Link to="" key={tag}><Button style={{paddingRight: "0.75em", paddingLeft: "0.75em"}} className="mx-1 badge rounded-pill" variant="secondary">{tag}</Button></Link>)}</div>
+                        {/* user, title, description, tags, like button is universal */
+                         /* post content is what differentiates different types of posts */
+                         /* feedback content is what is under the post, being comments, answers, group chat, etc. */}
+                    </Container>
+                </LinkContainer>
                 {!this.state.preview &&
                     <Container className="container-md mt-4">
                         <this.feedbackContent {...this.props} />
--- client/src/components/post/types/StudyGroupPost.js
+++ client/src/components/post/types/StudyGroupPost.js
@@ -7,7 +7,8 @@ import {Button} from "react-bootstrap";
 export default class StudyGroupPost extends Post {
     constructor(props) {
         super(props, {
-            joined: !!(props.joined)
+            joined: !!(props.joined),
+            grayed: props.flair === "full"
         });
         this.GroupPreview = this.GroupPreview.bind(this);
     }
@@ -21,7 +22,7 @@ export default class StudyGroupPost extends Post {
     postContent(props) {
         return (
             <>
-                <span className="text-tertiary d-flex align-items-center gap-1"><IoMdCalendar size="1.25em" /><span>{props.date}</span><strong className="px-1">&middot;</strong><MdLocationOn size="1.25em" /><span>{props.location}</span></span>
+                <span className={`${this.state.grayed ? "text-quaternary" : "text-tertiary"} d-flex align-items-center gap-1`}><IoMdCalendar size="1.25em" /><span>{props.date}</span><strong className="px-1">&middot;</strong><MdLocationOn size="1.25em" /><span>{props.location}</span></span>
                 <this.GroupPreview {...props} />
             </>
         );
@@ -39,14 +40,19 @@ export default class StudyGroupPost extends Post {
                         })
                     }
                 </div>
-                <span className="text-tertiary px-1">{props.members.length}/{props.limit} spots taken</span>
-                <Button onClick={() => {
+                <span className={`${this.state.grayed ? "text-quaternary" : "text-tertiary"} px-1`}>{props.members.length}/{props.limit} spots taken</span>
+                {(props.members.length < props.limit || this.state.joined) && <Button onClick={() => {
                     if (!this.state.joined) {
+                        props.members.push({
+                            icon: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
+                            username: "username",
+                            flair: "student"
+                        });
                         // logic for actually joining a group (backend link)
                         this.setState({joined: true});
                     }
                 }
-                } variant="success" className="rounded-pill px-3 py-1 mx-1" disabled={this.state.joined}>{this.state.joined ? "Joined" : "Join"}</Button>
+                } variant="success" className="rounded-pill px-3 py-1 mx-1" disabled={this.state.joined}>{this.state.joined ? "Joined" : "Join"}</Button>}
             </div>
         );
     }
